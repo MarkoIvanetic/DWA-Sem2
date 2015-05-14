87b071f20c079adb03d7eaf1059ee4d7f5695bc5
MarkoIvanetic <manna.grad@gmail.com>
2015-05-14 02:08:29 +0200

    Dynamic ad appending made in jQuery

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@ $(function(){
   $('#nav').click(function() {
     $(this).toggleClass('open');
   });
+
+  $("#click").click(function(){
+  	$(".ads-front-container").append('<div class="ads-front col-sm-6 col-md-4"></div>');
+  });
 });
