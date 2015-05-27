b0129b577fac03a1c9a637bc0de4d704e7f84c11
MarkoIvanetic <manna.grad@gmail.com>
2015-05-28 01:32:30 +0200

    Kinda drunk, dont know what I'm commiting but it genious!

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-  session_start();
-  include 'includes/connection.php';
-  ?>
+session_start();
+include 'includes/connection.php';
+?>
 <!doctype html>
 <html lang=''>
 <head>
@@ -19,7 +19,7 @@
  <script src="js/script.js"></script>
  <script src="js/JQscript.js"></script>
  <!--CSS-->
- <link rel="stylesheet" href="css/style.css">
+ 
  <link rel="stylesheet" href="css/normalize.css">
  <link href="css/bootstrap.css" rel="stylesheet">
   <!--[if lt IE 9]>
@@ -34,21 +34,21 @@
       <div id='cssmenu' class='align-right'>
         <ul>
          <!-- <li class='has-sub'><a href="#" id="user-profile"> -->
-          <?php 
-          if(isset($_SESSION['username']))
-            {echo " <li class='has-sub'><a href='#'' id='user-profile'>" . $_SESSION['username'] . "</a>";
-              echo "<ul><li><a href='profile.php'>My profile</a></li><li><a href='moji-oglasi.php'>My oglasi</a></li><li id='logout'><a href='logout.php'>Logout</a></li></ul></li>";
-        }
-          if(!isset($_SESSION['username'])) 
-            { echo '<li><a href="prijava.php">LOGIN</a></li>';} 
-          ?>
-        
-     <li><a href='#'>CONTACT</a></li>
-     <li><a href='predaja-oglasa.php'>PREDAJ OGLAS</a></li>
-     <li><a href='#'>ABOUT</a></li>
-     <li class='active'><a href='index.php'>HOME</a></li>
-   </ul>
- </div>
+         <?php 
+         if(isset($_SESSION['username']))
+          {echo " <li class='has-sub'><a href='#'' id='user-profile'>" . $_SESSION['username'] . "</a>";
+        echo "<ul><li><a href='profile.php'>My profile</a></li><li><a href='moji-oglasi.php'>My oglasi</a></li><li id='logout'><a href='logout.php'>Logout</a></li></ul></li>";
+      }
+      if(!isset($_SESSION['username'])) 
+        { echo '<li><a href="prijava.php">LOGIN</a></li>';} 
+      ?>
+
+      <li><a href='#' id="click">CONTACT</a></li>
+      <li><a href='predaja-oglasa.php'>PREDAJ OGLAS</a></li>
+      <li><a href='#'>ABOUT</a></li>
+      <li class='active'><a href='index.php'>HOME</a></li>
+    </ul>
+  </div>
 </header>
 <div class="banner darken wrap">
  <img src="res\bannerCLFlippednReady.png" class="">
@@ -60,15 +60,48 @@
 </div>
 
 <div class="content col-xs-12">
+
   <div class="ads-front-container col-sm-offset-1 col-sm-10">
+
     <div class="ads-front col-sm-6 col-md-4">
       <h2 class="col-xs-12">Dajem kornjaču i akvarij</h2>
       <h3 class="col-xs-12">Zagreb</h3>
       <p class="col-xs-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consectetur egestas risus ut tempor. Praesent eu fringilla nisl. Nullam blandit id nisi ac dapibus. Pellentesque laoreet, nisi vel mollis posuere, mi est fermentum mauris, quis tempus libero sem ullamcorper nisi.</p>
       <button class="red-button-front red-button col-xs-6">Više</button>
     </div>
+
+    <?php
+    $sql = "SELECT * FROM oglasi ORDER BY `id` DESC";
+    $result = mysqli_query($db, $sql);
+    while($row = mysqli_fetch_array($result))
+    {
+    // echo $row['id']; //ID oglasa
+    // echo '</br>' .$row['owner']; //vlasnik oglasa
+    // echo '</br>' .$row['title']; //Naslov 
+    // echo '</br>' .$row['description']; //Opis
+    // echo '</br>' .$row['keywords']; //Ključne rijeci
+    // echo '</br>' .$row['contact']; //Kontakt
+    // echo '</br>' .$row['date']; //Datum
+
+      echo '<div class="ads-front col-sm-6 col-md-4">';
+      echo '<h2 class="col-xs-12">' .$row['title']. '</h2>';
+      echo '<h3 class="col-xs-12">Predao: '.$row['owner'].'</h3>';
+      echo '<p class="col-xs-12">'.$row['description'].'</p>';
+
+    //Stvaranje URL-a
+      $id = $row['id'];
+      $_SESSION['oglas_id'] = $id;
+      $url = 'detalji-oglasa.php?id=' . $id;
+      echo '<button class="red-button red-button-front col-xs-6"><a href="' . $url . '">Više</a></button>';
+      echo '</div>';
+    }
+
+    ?> 
+
+
   </div>
 </div>
 
 </body>
+<link rel="stylesheet" href="css/style.css">
 <html>
